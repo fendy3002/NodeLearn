969cf3aa3acc517f1e0ffaffa545e0a5fa5762e3
Fendy Heryanto <fendy3002@yahoo.com>
2018-05-02 22:10:16 +0700

    add n3

---
+++ Es6/n3_array_deconstructor.js
@@ -0,0 +1,14 @@
+let initialData = {
+    "one": "1",
+    "two": "2",
+    "three": "3"
+};
+
+// the syntax below
+let {one, two} = initialData;
+
+// is the same as below
+let one = initialData.one;
+let two = initialData.two;
+
+// that's it
