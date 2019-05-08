80b98b24e282642e1dcc1520ddda9f3fc2d95e05
Fendy Heryanto <fendy3002@yahoo.com>
2019-05-08 22:24:39 +0700

    routington example

---
+++ Routington/app.js
@@ -0,0 +1,17 @@
+const routington = require('routington');
+const router = routington();
+
+let route = router.define('/order');
+route[0].label = "order_list";
+
+route = router.define('/order/:id');
+route[0].label = "order_view";
+
+let listMatch = router.match('/order');
+console.log("listMatch", listMatch);
+
+let viewMatch = router.match('/order/32');
+console.log("viewMatch", viewMatch);
+
+let noMatch = router.match('/noMatch/32');
+console.log("noMatch", noMatch);
