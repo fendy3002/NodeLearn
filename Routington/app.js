c2c9b4474af1877cd2f64285dd548ef1af3685f5
Fendy Heryanto <fendy3002@yahoo.com>
2019-05-11 10:09:22 +0700

    log param

--- Routington/app.js
+++ Routington/app.js
@@ -12,6 +12,7 @@ console.log("listMatch", listMatch);
 
 let viewMatch = router.match('/order/32');
 console.log("viewMatch", viewMatch);
+console.log("param", viewMatch.param);
 console.log("label", viewMatch.node.label);
 
 let noMatch = router.match('/noMatch/32');
