bf48b3861e6b1d931724017482b56c5c35dde766
Fendy Heryanto <fendy.heryanto@coderscolony.com>
2019-06-18 17:29:11 +0700

    dev

--- Nock/test.js
+++ Nock/test.js
@@ -1,34 +1,81 @@
+const assert = require('assert');
 const sa = require('superagent');
 const nock = require('nock');
 const hostname = 'http://example.com';
 
 describe('NOCK', function() {
-    it('it should match request header', function(done){
-        nock(hostname, {
-            reqheaders: {
-                "x-login-account": "SECOND"
-            }
-        }).get('/call').reply(200, function(uri, request, cb) {
+    it('it should match request header for get', function(done){
+        nock(hostname)
+        .matchHeader("x-login-account", "SECOND")
+        .persist()
+        .get('/call').reply(200, function(uri, request, cb) {
             cb(null, {
                 result: "second"
             });
         });
-        nock(hostname, {
-            reqheaders: {
-                "x-login-account": "FIRST"
-            }
-        }).get('/call').reply(200, function(uri, request, cb) {
+        nock(hostname)
+        .matchHeader("x-login-account", "FIRST")
+        .persist()
+        .get('/call').reply(200, function(uri, request, cb) {
             cb(null, {
                 result: "first"
             });
         });
 
         sa.get(hostname + '/call')
-        .set("x-login-account", "FIRST")
+        .set("x-login-account", "SECOND")
         .set('Content-Type', 'application/json')
         .end((err, res) => {
-            console.log(res.body);
-            done();
+            assert.equal(res.body.result, "second");
+            sa.get(hostname + '/call')
+            .set("x-login-account", "FIRST")
+            .set('Content-Type', 'application/json')
+            .end((err, res) => {
+                assert.equal(res.body.result, "first");
+                done();
+            });
+        });
+    })
+
+    it('it should match request header for post', function(done){
+        nock(hostname)
+        .matchHeader("x-login-account", "SECOND")
+        .persist()
+        .post('/call').reply(200, function(uri, request, cb) {
+            cb(null, {
+                result: "second"
+            });
+        });
+        nock(hostname)
+        .matchHeader("x-login-account", "FIRST")
+        .persist()
+        .post('/call').reply(200, function(uri, request, cb) {
+            cb(null, {
+                result: "first"
+            });
+        });
+
+        sa.post(hostname + '/call')
+        .set("x-login-account", "SECOND")
+        .set("x-login-password", "asdasdsa")
+        .set('Content-Type', 'application/json')
+        .send({
+            "my": "my",
+            "value": "value"
         })
+        .end((err, res) => {
+            assert.equal(res.body.result, "second");
+            sa.post(hostname + '/call')
+            .set("x-login-account", "FIRST")
+            .set('Content-Type', 'application/json')
+            .send({
+                "my": "my",
+                "value": "value"
+            })
+            .end((err, res) => {
+                assert.equal(res.body.result, "first");
+                done();
+            });
+        });
     })
 });
