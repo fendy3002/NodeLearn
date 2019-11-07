be30415cd2ade76724609d4e256a0156488dcdad
Fendy Heryanto <fendy.heryanto@valista-indonesia.com>
2019-11-07 10:03:02 +0700

    update superagent

---
+++ Superagent/testAgent.js
@@ -0,0 +1,14 @@
+let sa = require('superagent');
+
+let agent = async () => {
+    console.log();
+    console.log("agent");
+    let agent = sa.agent();
+    agent.set("Content-Type", "application/json");
+    agent.set("Authorization", "Bearer 12344567");
+
+    let res = await agent.post('https://httpbin.org/post')
+        .send({ "Data1": "Value1", "Data2": "Value2" })
+    console.log(res.body);
+};
+agent();
