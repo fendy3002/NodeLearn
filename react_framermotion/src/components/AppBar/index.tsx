ff805f6ee5e25c7d79736a0d47ace25703264ffd
Fendy Heryanto <fendy3002@gmail.com>
2022-07-07 11:42:04 +0800

    add styled components

--- react_framermotion/src/components/AppBar/index.tsx
+++ react_framermotion/src/components/AppBar/index.tsx
@@ -1,23 +1,52 @@
 import React from 'react';
 
 import { motion } from 'framer-motion';
+import styled from 'styled-components';
 
 import { AppBarProps } from '_/types/props/AppBarProps';
 
 import { GREY_900 } from '../../constants/colors';
 
+const StyledAppBarContainer = styled.div`
+  text-align: center;
+  color: ${GREY_900};
+  padding-top: 10px;
+  text-transform: uppercase;
+  padding-bottom: 10px;
+  background: linear-gradient(
+    180deg,
+    rgba(0, 0, 0, 0.3) 0%,
+    rgba(0, 0, 0, 0.1) 50%,
+    rgba(255, 255, 255, 0) 100%
+  );
+
+  & a:not(:last-child) {
+    margin-right: 32px;
+  }
+`;
+
 const A = (props: { href: string; children?: any }) => {
   return (
     <motion.a
       whileHover={{
-        scale: 1.6,
-        filter: 'brightness(90%)',
+        filter: [
+          'brightness(100%)',
+          'brightness(90%)',
+          'brightness(80%)',
+          'brightness(90%)',
+        ],
         textDecoration: 'underline',
         transition: {
-          duration: 0.3,
+          duration: 0.4,
+          type: 'keyframes',
         },
       }}
+      initial={{
+        filter: 'brightness(100%)',
+        fontSize: '2em',
+      }}
       style={{
+        fontSize: '2em',
         color: 'inherit',
         textDecoration: 'none',
         textShadow: '2px 2px 4px #000',
@@ -31,21 +60,10 @@ const A = (props: { href: string; children?: any }) => {
 
 export const AppBar = (props: AppBarProps) => {
   return (
-    <div
-      style={{
-        textAlign: 'center',
-        fontSize: '2em',
-        color: GREY_900,
-        paddingTop: '10px',
-        textTransform: 'uppercase',
-        paddingBottom: '10px',
-        background:
-          'linear-gradient(180deg, rgba(0, 0, 0, 0.3) 0%, rgba(0, 0, 0, 0.1) 50%, rgba(255,255,255,0) 100%)',
-      }}
-    >
+    <StyledAppBarContainer>
       <A href={'#'}>Skills</A>&nbsp;&nbsp;
       <A href={'#'}>Projects</A>&nbsp;&nbsp;
       <A href={'#'}>Contact me</A>
-    </div>
+    </StyledAppBarContainer>
   );
 };
