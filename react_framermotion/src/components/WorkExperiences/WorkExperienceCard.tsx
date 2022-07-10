2200754f5c860fd4614f977e703582457272dd46
Fendy Heryanto <fendy3002@gmail.com>
2022-07-10 15:26:59 +0800

    work experience action now for whole card

--- react_framermotion/src/components/WorkExperiences/WorkExperienceCard.tsx
+++ react_framermotion/src/components/WorkExperiences/WorkExperienceCard.tsx
@@ -1,7 +1,13 @@
 import { useState } from 'react';
 
 import { ArrowDropDown, ArrowDropUp } from '@mui/icons-material';
-import { Button, Card, CardContent } from '@mui/material';
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+} from '@mui/material';
 
 import { motion } from 'framer-motion';
 
@@ -14,49 +20,45 @@ export const WorkExperienceCard = (props: {
   const [expanded, setExpanded] = useState(false);
   return (
     <Card style={{ marginBottom: '16px', background: 'rgba(255,255,255,0.3)' }}>
-      <CardContent
-        style={{
-          padding: '0px',
-          textAlign: 'left',
-          background: 'transparent',
-        }}
-      >
-        <div
+      <CardActionArea onClick={() => setExpanded(!expanded)}>
+        <CardContent
           style={{
-            paddingTop: '16px',
-            paddingRight: '16px',
-            paddingLeft: '16px',
-            paddingBottom: '16px',
+            padding: '0px',
+            textAlign: 'left',
+            background: 'transparent',
           }}
         >
-          <h3 style={{ marginTop: '0px', marginBottom: '4px' }}>
-            {props.company}
-          </h3>
           <div
             style={{
-              display: 'flex',
+              paddingTop: '16px',
+              paddingRight: '16px',
+              paddingLeft: '16px',
+              paddingBottom: '16px',
             }}
           >
-            {props.location}
-            <small style={{ marginLeft: 'auto' }}>{props.when}</small>
+            <h3 style={{ marginTop: '0px', marginBottom: '4px' }}>
+              {props.company}
+            </h3>
+            <div
+              style={{
+                display: 'flex',
+              }}
+            >
+              {props.location}
+              <small style={{ marginLeft: 'auto' }}>{props.when}</small>
+            </div>
+            {expanded && (
+              <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+                {props.detail}
+              </motion.div>
+            )}
           </div>
-          {expanded && (
-            <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
-              {props.detail}
-            </motion.div>
-          )}
-        </div>
-        <div style={{ display: 'flex' }}>
-          <Button
-            style={{ width: '100%' }}
-            size={'small'}
-            onClick={() => setExpanded(!expanded)}
-          >
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
             {!expanded && <ArrowDropDown />}
             {expanded && <ArrowDropUp />}
-          </Button>
-        </div>
-      </CardContent>
+          </div>
+        </CardContent>
+      </CardActionArea>
     </Card>
   );
 };
