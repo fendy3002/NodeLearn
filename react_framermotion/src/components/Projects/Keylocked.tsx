930c3f176f9e85daf1b50ca368c0c2fcf246e94b
Fendy Heryanto <fendy3002@gmail.com>
2022-07-10 16:29:41 +0800

    remove keylocked debug

--- react_framermotion/src/components/Projects/Keylocked.tsx
+++ react_framermotion/src/components/Projects/Keylocked.tsx
@@ -9,12 +9,10 @@ import { motion } from 'framer-motion';
 export const Keylocked = (props: { children: any }) => {
   const [selectedNumber, setSelectedNumber] = useState('789');
   // debug to true
-  const isUnlocked = true; //selectedNumber == '777';
+  const isUnlocked = selectedNumber == '777';
   return (
     <>
-      <div style={{ fontSize: '1.2em', marginBottom: '20px' }}>
-        Set to "777" to unlock
-      </div>
+      <div style={{ marginBottom: '20px' }}>Set to "777" to unlock</div>
       <div>
         <Keylock
           selectedNumber={selectedNumber}
